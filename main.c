aa7d7dc7417ad6a12ade9894cab9a0ae939516d2
Luis Cabrera Benito <parzibyte@gmail.com>
2020-06-07 20:36:14 -0500

    Contando minas alrededor

--- main.c
+++ main.c
@@ -3,14 +3,56 @@
 #include <stdio.h>
 #include <stdlib.h>  // rand
 #include <unistd.h>  // getpid
-#define COLUMNAS 7
-#define FILAS 7
+#define COLUMNAS 4
+#define FILAS 4
 #define ESPACIO_SIN_DESCUBRIR '?'
 #define ESPACIO_DESCUBIERTO ' '
 #define MINA 'M'
 #define CANTIDAD_MINAS 5
 #define ERROR_MINA_ENCONTRADA 1
 #define ERROR_ESPACIO_YA_DESCUBIERTO 2
+#define ERROR_NINGUNO 3
+#define DEBUG 1
+
+int obtenerMinasCercanas(int fila, int columna, char tablero[FILAS][COLUMNAS]) {
+  int conteo = 0, filaInicio, filaFin, columnaInicio, columnaFin;
+  if (fila <= 0) {
+    filaInicio = 0;
+  } else {
+    filaInicio = fila - 1;
+  }
+
+  if (fila + 1 >= FILAS) {
+    filaFin = FILAS - 1;
+  } else {
+    filaFin = fila + 1;
+  }
+
+  if (columna <= 0) {
+    columnaInicio = 0;
+  } else {
+    columnaInicio = columna - 1;
+  }
+  if (columna + 1 >= COLUMNAS) {
+    columnaFin = COLUMNAS - 1;
+  } else {
+    columnaFin = columna + 1;
+  }
+  // printf("FI: %d FF: %d CI: %d CF: %d", filaInicio, filaFin, columnaInicio,
+  //        columnaFin);
+
+  int m;
+  for (m = filaInicio; m <= filaFin; m++) {
+    int l;
+    for (l = columnaInicio; l <= columnaFin; l++) {
+      // printf("tablero[m][l]=%c\n", tablero[m][l]);
+      if (tablero[m][l] == MINA) {
+        conteo++;
+      }
+    }
+  }
+  return conteo;
+}
 
 // Devuelve un número aleatorio entre minimo y maximo, incluyendo a minimo y
 // maximo
@@ -29,12 +71,16 @@ void iniciarTablero(char tablero[FILAS][COLUMNAS]) {
   }
 }
 
+void colocarMina(int fila, int columna, char tablero[FILAS][COLUMNAS]) {
+  tablero[fila][columna] = MINA;
+}
+
 void colocarMinas(char tablero[FILAS][COLUMNAS]) {
   int l;
   for (l = 0; l < CANTIDAD_MINAS; l++) {
     int fila = aleatorio_en_rango(0, FILAS - 1);
     int columna = aleatorio_en_rango(0, COLUMNAS - 1);
-    tablero[fila][columna] = MINA;
+    colocarMina(fila, columna, tablero);
   }
 }
 
@@ -73,6 +119,10 @@ void imprimirEncabezado() {
   printf("\n");
 }
 
+char enteroACaracter(int numero) {
+  return numero + '0';
+}
+
 void imprimirTablero(char tablero[FILAS][COLUMNAS]) {
   imprimirEncabezado();
   imprimirSeparadorEncabezado();
@@ -83,7 +133,20 @@ void imprimirTablero(char tablero[FILAS][COLUMNAS]) {
     printf("| %c ", letra);
     letra++;
     for (m = 0; m < COLUMNAS; m++) {
-      printf("| %c ", tablero[l][m]);
+      char verdaderaLetra = ESPACIO_SIN_DESCUBRIR;
+      char letraActual = tablero[l][m];
+      if (letraActual == MINA) {
+        verdaderaLetra = ESPACIO_SIN_DESCUBRIR;
+      } else if (letraActual == ESPACIO_DESCUBIERTO) {
+        int minasCercanas = obtenerMinasCercanas(l, m, tablero);
+        // printf("Minas: %d", minasCercanas);
+        verdaderaLetra = enteroACaracter(minasCercanas);
+      }
+
+      if (letraActual == MINA && DEBUG) {
+        verdaderaLetra = MINA;
+      }
+      printf("| %c ", verdaderaLetra);
       if (m + 1 == COLUMNAS) {
         printf("|");
       }
@@ -109,6 +172,7 @@ int abrirCasilla(char filaLetra, int columna, char tablero[FILAS][COLUMNAS]) {
     return ERROR_ESPACIO_YA_DESCUBIERTO;
   }
   tablero[fila][columna] = ESPACIO_DESCUBIERTO;
+  return ERROR_NINGUNO;
 }
 
 int main() {
@@ -116,11 +180,22 @@ int main() {
   // Alimentar rand
   srand(getpid());
   iniciarTablero(tablero);
-  colocarMinas(tablero);
-  imprimirTablero(tablero);
+  colocarMina(1, 1, tablero);
+  colocarMina(0, 1, tablero);
+  colocarMina(1, 0, tablero);
   abrirCasilla('A', 1, tablero);
-  imprimirTablero(tablero);
-  abrirCasilla('G', 7, tablero);
+  abrirCasilla('A', 3, tablero);
+  abrirCasilla('A', 4, tablero);
+  abrirCasilla('B', 3, tablero);
+  abrirCasilla('B', 4, tablero);
+  abrirCasilla('C', 1, tablero);
+  abrirCasilla('C', 2, tablero);
+  abrirCasilla('C', 3, tablero);
+  abrirCasilla('C', 4, tablero);
+  abrirCasilla('D', 1, tablero);
+  abrirCasilla('D', 2, tablero);
+  abrirCasilla('D', 3, tablero);
+  abrirCasilla('D', 4, tablero);
   imprimirTablero(tablero);
   //   while (1) {
   //       int fila, columna;
