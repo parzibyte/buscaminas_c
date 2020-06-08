f9d5e4940f97aafbe2d366550173913608787cc2
Luis Cabrera Benito <parzibyte@gmail.com>
2020-06-07 21:30:44 -0500

    Créditos

--- main.c
+++ main.c
@@ -1,4 +1,22 @@
 /*
+  ____          _____               _ _           _
+ |  _ \        |  __ \             (_) |         | |
+ | |_) |_   _  | |__) |_ _ _ __ _____| |__  _   _| |_ ___
+ |  _ <| | | | |  ___/ _` | '__|_  / | '_ \| | | | __/ _ \
+ | |_) | |_| | | |  | (_| | |   / /| | |_) | |_| | ||  __/
+ |____/ \__, | |_|   \__,_|_|  /___|_|_.__/ \__, |\__\___|
+         __/ |                               __/ |
+        |___/                               |___/
+
+    Blog:       https://parzibyte.me/blog
+    Ayuda:      https://parzibyte.me/blog/contrataciones-ayuda/
+    Contacto:   https://parzibyte.me/blog/contacto/
+
+    Copyright (c) 2020 Luis Cabrera Benito
+    Licenciado bajo la licencia MIT
+
+    El texto de arriba debe ser incluido en cualquier redistribución
+
   Errores esperados, documentación y esas cosas...
   - No puede haber más filas que letras del abecedario, porque solo se imprime
   hasta la Z
@@ -6,6 +24,7 @@
   ya no se convertiría correctamente el entero a cadena para mostrar las minas
   cercanas
 */
+
 #include <assert.h>
 #include <ctype.h>  // toupper
 #include <stdio.h>
@@ -193,6 +212,7 @@ int noHayCasillasSinAbrir(char tablero[FILAS][COLUMNAS]) {
 }
 
 int main() {
+  printf("** BUSCAMINAS **\nBy Parzibyte\n");
   char tablero[FILAS][COLUMNAS];
   int deberiaMostrarMinas = 0;
   // Alimentar rand
